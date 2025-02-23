351543933e54ba564af9fd71366a77867139ddc2
khanh-desgin <buiduykhanh1062003@gmail.com>
2025-02-22 19:37:41 -0700

    first commit

---
+++ Lession15/BaiTap/Bai3/ExceptionTriangle/IIIlegalTriangleException.java
@@ -0,0 +1,7 @@
+package BaiTap.Bai3.ExceptionTriangle;
+
+public class IIIlegalTriangleException extends Exception {
+    public IIIlegalTriangleException(String message) {
+        super(message);
+    }
+}
