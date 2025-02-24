23e05d58440ae313585e747f97270cda56c81b44
khanh-desgin <buiduykhanh1062003@gmail.com>
2025-02-24 06:00:35 -0700

    first commit

--- Lession15/BaiTap/Bai3/IllegalTriangleException.java
+++ Lession15/BaiTap/Bai3/IllegalTriangleException.java
@@ -3,33 +3,37 @@ package BaiTap.Bai3;
 import java.util.Scanner;
 
 public class IllegalTriangleException extends Throwable {
-    public static void checkTriangle(int a, int b, int c) throws IllegalTriangleException {
-        if (a <= 0 || b <= 0 || c <= 0) {
-            throw new IllegalTriangleException();
-        }
-        if (a + b <= c || a + c <= b || b + c <= a) {
-            throw new IllegalTriangleException();
-        }
-    }
+//    public static void checkTriangle(int a, int b, int c) throws IllegalTriangleException {
+//        if (a <= 0 || b <= 0 || c <= 0) {
+//            throw new IllegalTriangleException();
+//        }
+//        if (a + b <= c || a + c <= b || b + c <= a) {
+//            throw new IllegalTriangleException();
+//        }
+//    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+//        try {
+//            System.out.print("Enter number1 of triangle: ");
+//            int number1 = sc.nextInt();
+//            System.out.print("Enter number2 of triangle: ");
+//            int number2 = sc.nextInt();
+//            System.out.print("Enter number3 of triangle: ");
+//            int number3 = sc.nextInt();
+//
+//            checkTriangle(number1, number2, number3);
+//            System.out.println("Ba canh hop le tao thanh 1 tam gia");
+//        } catch (IllegalTriangleException e) {
+//            System.out.println("Error: " + e.getMessage());
+//        } catch (Exception e) {
+//            System.out.println("Loi dau vao khong hop le");
+//        } finally {
+//            sc.close();
+//        }
         try {
-            System.out.print("Enter number1 of triangle: ");
-            int number1 = sc.nextInt();
-            System.out.print("Enter number2 of triangle: ");
-            int number2 = sc.nextInt();
-            System.out.print("Enter number3 of triangle: ");
-            int number3 = sc.nextInt();
-
-            checkTriangle(number1, number2, number3);
-            System.out.println("Ba canh hop le tao thanh 1 tam gia");
-        } catch (IllegalTriangleException e) {
-            System.out.println("Error: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Loi dau vao khong hop le");
+            return;
         } finally {
-            sc.close();
+            System.out.println("error");
         }
-
     }
 }
--- Lession16/DemoFileText.java
+++ Lession16/DemoFileText.java
@@ -1,15 +1,39 @@
-import java.io.File;
+import java.io.*;
 
 public class DemoFileText {
     public static void main(String[] args) {
-        File newFile = new File("data.txt");
+
+        File newFile = new File("E:\\c1024l1_jv103\\Lession16\\data.txt");
+        //kiem tra file tong tai
+//        System.out.println(newFile.canWrite());
+//        System.out.println(newFile.canRead()); // doc file
+//        System.out.println(newFile.getName()); // lay ten
+//        System.out.println(newFile.getAbsolutePath()); // lay duong dan
+//        System.out.println(newFile.getParent()); // lay duong dan tuyet doi
+
+//        newFile.delete(); // xoa file
+//        newFile.mkdir(); // tao thu muc
 
 
+        // Ghi du lieu vao file
+        try {
+            FileWriter fileWriter = new FileWriter(newFile, true);
+
+            //ghi: fileWrite vaf BuffedWriter
+
+//            fileWriter.write("Hello World \n");
+//            fileWriter.write("Hello World \n");
+            fileWriter.write("Minh");
+
+            fileWriter.close(); // dong luong ghi
+        } catch (IOException e) {
+            System.out.println("error writing to file: " + e.getMessage());
+        }
     }
 
-    public static void chenkFile(File file) {
+    public static void chenkFile(File file) throws FileNotFoundException {
         if (!file.exists()) {
-            throw new IllegalStateException("erorr");
+            throw new FileNotFoundException("File " + file.getAbsoluteFile());
         }
     }
 }
